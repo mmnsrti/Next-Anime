94eebe63c63139b5a42532477e9365cb32ba866d
mmnsrti <mmnsrti@gmail.com>
2024-02-16 18:06:06 +0330

    change the set data to be functional update

--- components/LoadMore.tsx
+++ components/LoadMore.tsx
@@ -24,7 +24,7 @@ function LoadMore() {
 
       const timeoutId = setTimeout(() => {
         fetchAnime(page).then((res) => {
-          setData([...data, ...res]);
+          setData((prevData) => [...prevData, ...res]); // Use functional update
           page++;
         });
 
