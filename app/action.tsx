819cbd59012494ae378b61b62e57e105d0fbe91f
mmnsrti <mmnsrti@gmail.com>
2024-02-16 16:49:40 +0330

    changed the api url

--- app/action.tsx
+++ app/action.tsx
@@ -1,9 +1,11 @@
 "use server"
 
 import AnimeCard, { AnimeProp } from "@/components/AnimeCard";
+const MAX_LIMIT = 8;
 
 export const fetchAnime = async(page:number) => {
-    const response =await  fetch(`https://shikimori.one/api/animes?page=${page}&limit=12&order=popularity`)
+    const response =await  fetch(    `https://shikimori.one/api/animes?page=${page}&limit=${MAX_LIMIT}&order=popularity`
+    )
     const data = await response.json()
      return data.map((item: AnimeProp, index:number) => (
         <AnimeCard key={item.id} anime={item} index={index} />
