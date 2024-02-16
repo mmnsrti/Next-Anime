0503ca4f31818d0bb01369628489be68d1b955bf
mmnsrti <mmnsrti@gmail.com>
2024-02-16 15:43:25 +0330

    added framer motion and change the div to be rendered on server and some of them on client side

--- components/AnimeCard.tsx
+++ components/AnimeCard.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import { MotionDiv } from "./MotionDiv";
 
 export interface AnimeProp {
   id: string;
@@ -17,9 +18,21 @@ interface Prop {
   index: number;
 }
 
-function AnimeCard({ anime }: Prop) {
+const variant = {
+  hidden:{opacity:0},
+  visible:{opacity:1}
+}
+
+
+function AnimeCard({ anime,index }: Prop) {
   return (
-    <div className="max-w-sm rounded relative w-full">
+    <MotionDiv
+      variants={variant}
+      animate="visible"
+      transition={{delay:index*0.25,ease:'easeInOut',duration:0.5}}
+      initial="hidden"
+      viewport={{amount:0}}
+      className="max-w-sm rounded relative w-full">
       <div className="relative w-full h-[37vh]">
         <Image
           src={`https://shikimori.one${anime.image.original}`}
@@ -64,7 +77,7 @@ function AnimeCard({ anime }: Prop) {
           </div>
         </div>
       </div>
-    </div>
+    </MotionDiv>
   );
 }
 
--- components/LoadMore.tsx
+++ components/LoadMore.tsx
@@ -1,10 +1,34 @@
+"use client";
+import { fetchAnime } from "@/app/action";
 import Image from "next/image";
+import { useEffect, useState } from "react";
+import { useInView } from "react-intersection-observer";
+import AnimeCard from "./AnimeCard";
 
+
+export type AnimeProp = JSX.Element
+
+let page =2
 function LoadMore() {
+  const [data, setData] = useState<AnimeProp[]>([]);
+  const { ref, inView, entry } = useInView({});
+  useEffect(() => {
+    if (inView) {
+      fetchAnime(page).then((res) => {
+        setData([...data, ...res]);
+        page++
+      });
+    }
+  }),
+    [inView,data];
+
   return (
     <>
+      <section className="grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-10">
+        {data}
+      </section>
       <section className="flex justify-center items-center w-full">
-        <div>
+        <div ref={ref}>
           <Image
             src="./spinner.svg"
             alt="spinner"
