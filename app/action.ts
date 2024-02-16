2d9c4a54512475e5c182550f606b0a6a054c4e8b
mmnsrti <mmnsrti@gmail.com>
2024-02-16 13:40:20 +0330

    added api call to shikimori and fixed the image loading

--- app/action.ts
+++ app/action.ts
@@ -1,5 +1,7 @@
 "use server"
-const fetchAnime = async()=>{
-    const response =await  fetch('https://api.jikan.moe/v3/search/anime?')
+export const fetchAnime = async(page:number) => {
+    const response =await  fetch(`https://shikimori.one/api/animes?page=${page}&limit=12&order=popularity`)
+    const data = await response.json()
+     return data;
 
 } 
